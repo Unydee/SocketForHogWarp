6823ade87873aa13c2d3f68a36a247ceb854754d
Edynu <weakluke@gmail.com>
2023-02-20 05:30:45 +0100

    test return revert

--- src/luasocket.c
+++ src/luasocket.c
@@ -112,7 +112,6 @@ static int base_open(lua_State *L) {
 \*-------------------------------------------------------------------------*/
 LUASOCKET_API int luaopen_socket_core(lua_State *L) {
     int i;
-    return 0;
     base_open(L);
     for (i = 0; mod[i].name; i++) mod[i].func(L);
     return 1;
